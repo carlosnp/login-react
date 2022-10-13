835c7fc267f219cfea7fe19ef2bd8a322b622d1d
Carlos Nava <machinery.e@gmail.com>
2022-10-13 16:02:12 -0400

    Crear el formulario del login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  /**
+   * Actualiza la variable del correo
+   * @param {*} event 
+   */
+  const handleEmailChange = ({target: {value}}) => {
+    setEmail(value);
+  };
+  /**
+   * Actualiza la variable de la contraseña
+   * @param {*} event 
+   */
+  const handlePasswordChange = (event) => {
+    setPass(event.target.value);
+    event.preventDefault();
+  };
+  /**
+   * Obtiene los datos del formulario
+   * @param {*} event 
+   */
+  const handleSubmit = (event) => {
+    const data = new FormData(event.target);
+    /** Datos del formulario */
+    const formObject = Object.fromEntries(data.entries());
+    console.log('formObject', formObject);
+    event.preventDefault();
+  };
+  /**
+   * Limpia el formulario
+   * @param {*} event 
+   */
+  const cleanAll = (event)=>{
+    setEmail('');
+    setPass('');
+    event.preventDefault();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <h2>Iniciar sesion</h2>
+        <label htmlFor="email">
+          Correo
+          <input type="email" name="Correo" id="email"
+            onChange={handleEmailChange}
+            value={email || ''}
+          />
+        </label>
+        <label htmlFor="password">
+          Contraseña
+          <input type="password" name="Contraseña" id="password"
+            onChange={handlePasswordChange}/>
+        </label>
+        <button type="submit">Continuar</button>
+        <button type="button" onClick={cleanAll}>Limpiar</button>
+      </form>
+      <div>Correo: {email}</div>
+      <div>Contraseña: {pass}</div>
     </div>
   );
 }
